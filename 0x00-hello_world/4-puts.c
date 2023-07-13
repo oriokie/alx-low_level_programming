2900001a2158719e6e82189da592531995a0529c
“Edwin <oriokie@icloud.com>
2023-07-13 12:44:27 +0300

    Lets go C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+*main - Entry Point*
+*
+*Return: Always Zero
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
