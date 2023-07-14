900786cfb58c024d4c1c68506042e22365a3ccbc
“Edwin <oriokie@icloud.com>
2023-07-15 00:26:59 +0300

    All

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  *
  * Description: Prints all possible combinations of two two-digit numbers,
  *              ranging from 0 to 99, in ascending order. The combinations
- *              are separated by a comma and space. Each number is printed
+ *              are separated by a comma and no space. Each number is printed
  *              with two digits. For example, 1 is printed as 01.
  *
  * Return: Always 0 (Success)
@@ -16,22 +16,16 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			if (num1 != num2)
-			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(',');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(',');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-				if (num1 != 98 || num2 != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			if (num1 != 98 || num2 != 99)
+				putchar(',');
 		}
 	}
 
@@ -39,4 +33,3 @@ int main(void)
 
 	return (0);
 }
-
