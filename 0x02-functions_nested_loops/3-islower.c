4a0a7d36b7fdee37911a36d3705b39b97333f710
“Edwin <oriokie@icloud.com>
2023-07-18 14:31:55 +0300

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _islower - Checks if a character is lower case
+*
+* @c: The Character to be checked
+*
+* Return: 1 if the character is lowercase
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
