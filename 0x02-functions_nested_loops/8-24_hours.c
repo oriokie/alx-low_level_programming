e7e85e5552fc883b109bd9a23cf66f8c693e4690
“Edwin <oriokie@icloud.com>
2023-07-19 01:11:35 +0300

    Jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* jack_bauer - function for printing every minute
+* Return: Always zero
+*/
+void jack_bauer(void);
+{
+	for (int a = 0; a <= 23, a++)
+	{
+		for (int b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+		}
+
+
+	}
+
+}
