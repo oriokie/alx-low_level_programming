9efca95a93d5b0112e9187bed4cb3b87d5619538
“Edwin <oriokie@icloud.com>
2023-07-19 00:19:35 +0300

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* int_abs - Function computes the abosulte value of an integer
+*@n: integer to be checked
+* Return: Always 0
+*/
+int_abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
