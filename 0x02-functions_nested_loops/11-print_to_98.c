195d28829b2adcd3fda9ee768e756cf29c42c782
“Edwin <oriokie@icloud.com>
2023-07-19 01:48:52 +0300

    Lets print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_to_98 - function to print natural numbers
+* @n: natural number passed to the function
+* Return: Always zero
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+
+	_putchar('98');
+}
