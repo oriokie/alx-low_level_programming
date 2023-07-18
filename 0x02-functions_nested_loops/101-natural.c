cb0233eb46db7f1881a06c5d244c194c05577e69
“Edwin <oriokie@icloud.com>
2023-07-19 02:18:05 +0300

    Lets do natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program for checking multiples
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int x = 1024, y, sum = 0;
+
+	for (y = 0; y < x; y++)
+	{
+		if ((y % 3 == 0) || (y % 5 == 0))
+		{
+			sum = sum + y;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
