104ff409b5f1aca416679efebd8f9c72127e60a9
“Edwin <oriokie@icloud.com>
2023-07-19 02:13:10 +0300

    Tough numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,26 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		while (n < 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			if (n < 0)
+			{
+				_putchar('-');
+				n = -n;
+			}
+
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+
 			_putchar(',');
 			_putchar(' ');
 			n++;
@@ -21,14 +35,27 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			if (n < 0)
+			{
+				_putchar('-');
+				n = -n;
+			}
+
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		}
 	}
-
 	_putchar('9');
 	_putchar('8');
 }
