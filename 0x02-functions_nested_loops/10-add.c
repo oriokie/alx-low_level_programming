e3ce3f581688ef652dfcf80aea833cc387ca32e9
“Edwin <oriokie@icloud.com>
2023-07-19 01:31:02 +0300

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#includes "main.h>
+/**
+* add - adds two integers
+* @a: first variable
+* @b: second variable
+* Return: Always Zero
+*/
+int add (int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+
+	return (sum);
+}
