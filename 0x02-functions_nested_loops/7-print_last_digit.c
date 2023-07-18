86207b51bdec50791e60c34733583c3a5bdbe271
“Edwin <oriokie@icloud.com>
2023-07-19 00:45:49 +0300

    Print last digit edited

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,14 @@ int print_last_digit(int n)
 	if (n >= 0)
 	{
 		last_digit = n % 10;
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 	else
 	{
 		n = -1 * n;
 		last_digit = n % 10;
+		_putchar (last_digit + '0');
 		return (last_digit);
 	}
 }
