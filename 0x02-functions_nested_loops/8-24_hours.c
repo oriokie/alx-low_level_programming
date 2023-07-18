15343c7b9d7a41c7040b3f0b0a6d6e275aa1347b
“Edwin <oriokie@icloud.com>
2023-07-19 01:17:39 +0300

    24 hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,11 @@
 */
 void jack_bauer(void)
 {
-	for (int a = 0; a <= 23; a++)
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
 	{
-		for (int b = 0; b <= 59; b++)
+		for (b = 0; b <= 59; b++)
 		{
 			_putchar (a / 10 + '0');
 			_putchar (a % 10 + '0');
