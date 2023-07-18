d316df2aa4babbaf151bbd1223a9028e64a8b1a7
“Edwin <oriokie@icloud.com>
2023-07-19 01:55:30 +0300

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,8 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			_putchar(n);
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			n++;
@@ -20,12 +21,14 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			_putchar(n);
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		}
 	}
 
-	_putchar('98');
+	_putchar('9');
+	_putchar('8');
 }
