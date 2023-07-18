b762730cdcc3ad7b6a3bd3523d1c55336232e56c
“Edwin <oriokie@icloud.com>
2023-07-19 00:29:18 +0300

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - returns the value of the last digit
+* @n: the value to be cheked
+* Return: Always zero
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
