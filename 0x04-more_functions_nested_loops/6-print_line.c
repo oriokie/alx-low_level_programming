24f41d249032901d92cfe57ec3d5dfcf578c3133
“Edwin <oriokie@icloud.com>
2023-07-20 10:27:29 +0300

    Lets print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - functions that prints a line
+* @n: the int describing the number of times
+* Return: no return needed for the function
+*/
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n')
+}
