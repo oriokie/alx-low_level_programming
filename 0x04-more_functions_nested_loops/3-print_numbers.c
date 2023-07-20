e4736aac1e707e3eb99da1b7204dc8b13a0c9f05
“Edwin <oriokie@icloud.com>
2023-07-20 09:35:37 +0300

    Lets print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - function for printing numbers 0 to 9
+* Return: Awlays zero
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0', i < '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
