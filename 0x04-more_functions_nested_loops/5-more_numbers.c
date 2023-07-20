80070df6094e97d24654a71a99d3ed301100981c
“Edwin <oriokie@icloud.com>
2023-07-21 00:07:39 +0300

    Lets go universal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@ void more_numbers(void)
 {
 	int i, n;
 
-	n = 0;
-	while (n <= 10)
+	n = '0';
+	while (n <= '10')
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = '0'; i <= '14'; i++)
 		{
-			if (i > 9)
+			if (i > '9')
 				_putchar('0' + (i / 10));
 				_putchar('0' + (i % 10));
 		}
