bfc21dfe739f947b75f98ceb9a69659e2cceae52
“Edwin <oriokie@icloud.com>
2023-07-20 10:16:32 +0300

    Lets to to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - prints 0 to 14 ten times
+* Return: Always zero
+*/
+void more_numbers(void)
+{
+	int i, n;
+
+	n = 0;
+	while (n <= 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+		}
+	_putchar('\n');
+	n++;
+	}
+}
