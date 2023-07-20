75f90c868359eb85102f8d48e503e715ffd099d3
“Edwin <oriokie@icloud.com>
2023-07-20 09:27:45 +0300

    lets multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @a: first int
+* @b: second int
+* Return: Always zero
+*/
+int mul(int a, int b)
+	multi = a * b;
+	return (multi);
+}
