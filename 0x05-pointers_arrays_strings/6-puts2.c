6dc12f1def1f503e498a236d837ed078e0ae38fd
“Edwin <oriokie@icloud.com>
2023-07-24 21:11:57 +0300

    Lorem ipsum

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i + 1] != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i += 2;
-		}
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
