6bd068dbcc9309ddebcd62a08f66f6dad27e7fbb
“Edwin <oriokie@icloud.com>
2023-07-24 09:02:30 +0300

    Pointer updating value to 98 from 402

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
 * reset_to_98 - functions that takes a pointer to an int
-* @*n
+* @n: parameter to be updated
 * Return: no return
 */
 void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
