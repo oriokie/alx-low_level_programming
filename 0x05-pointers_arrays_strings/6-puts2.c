170e20e3d18fab3554c62cbb7e1d3b5d986201e4
“Edwin <oriokie@icloud.com>
2023-07-24 23:43:49 +0300

    Does it work?

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,20 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (str[i] != '\0' && str[i + 1] != '\')
+	while (count >= 0)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (str[count] == '\n')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2)
+		{
+			_putchar(str[count]);
+			count++;
+		}
 	}
-	_putchar('\n');
 }
+
