7b0eb38398289a8136b403b459c8f477e7c7e089
“Edwin <oriokie@icloud.com>
2023-07-24 10:21:22 +0300

    Edited the length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
 */
 void print_rev(char *s)
 {
-	int length = 1;
+	int length = 0;
 	int i;
 
 	while (s[length] != '\0')
 		length++;
-	for (i = length - 1; i >= 0; i--)
+	for (i = length; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
