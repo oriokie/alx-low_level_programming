6f826a4c0e62b81d9f7b0d142d6d40dc6f698957
“Edwin <oriokie@icloud.com>
2023-07-24 20:54:16 +0300

    Lets print characters in alternate

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every char of a str
+ * @str: string to be printed
+ * Return: none
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+}
