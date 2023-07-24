8004745059ac9f33995fc1af7a1e583f1db7f161
“Edwin <oriokie@icloud.com>
2023-07-24 20:40:57 +0300

    revised the code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,15 @@
 void rev_string(char *s)
 {
 	int length = 0;
-	int i;
+	int i, j;
 
 	while (s[length] != '\0')
 		length++;
-	for (i = length - 1; i >= 0; i--)
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-		_putchar(s[i]);
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = temp;
 	}
 }
