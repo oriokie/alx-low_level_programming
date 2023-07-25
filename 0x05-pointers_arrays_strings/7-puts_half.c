08c144c5dc80bdd0368b0626a3da482525cd3337
“Edwin <oriokie@icloud.com>
2023-07-25 08:48:30 +0300

    Edited

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,33 +6,26 @@
 */
 void puts_half(char *str)
 {
-	int start, len;
+	int start, count = 0;
 
-	len = _strlen(str);
-
-	if (len % 2 == 0)
-		start = len / 5;
+	while (*str != '\0')
+	{
+		count++;
+		s++;
+	}
+	if (count % 2 == 0)
+	{
+		start = count / 2;
 
 		for (; *str != '\0'; start++)
 			_putchar(str[start]);
+	}
 	else
+	{
 		start = (len - 1) / 2;
 
 		for (; *str != '\0'; start++)
 			_putchar(str[start]);
+	}
 	_putchar('\n');
-}
-/**
-* _strlen - Returns the length of a string.
-* @s: The string to be counted.
-* Return: The length of the string.
-*/
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-		count++;
-		s++;
-	return (count);
 }
