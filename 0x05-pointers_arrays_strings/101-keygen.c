21566fe5223ba2f1c865a02830155825047fced3
“Edwin <oriokie@icloud.com>
2023-07-25 10:38:03 +0300

    Lets see

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* getRandomChar - generates password characters
+* generatepass - generates password
+* main - generates keygen
+*/
+char getRandomChar(void)
+{
+	return ('A' + rand() % 26);
+}
+
+void generatepass(int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		printf("%c", getRandomChar());
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int passwordLength = 10;
+
+	srand((unsigned int)time(NULL));
+	generatepass(passwordLength);
+	return (0);
+}
