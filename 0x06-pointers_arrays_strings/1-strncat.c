0c92a7c391d035b8510ed566e60289ac4c1fae6a
“Edwin <oriokie@icloud.com>
2023-07-26 09:58:07 +0300

    Edited the while loop

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,18 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	/* initializing variables */
-	int i = 0, int j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 	while (src[j] != '\0')
 	{
 		if (j < n)
-		{
 			dest[i] = src[j];
 			j++;
 			i++;
-		}
+	}
 	dest[i] = '\0';
 	return (dest);
 }
