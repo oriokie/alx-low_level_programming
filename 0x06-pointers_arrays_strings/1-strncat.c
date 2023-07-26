bc7886b201b61a814e70771ad743ab5f012d9c3c
“Edwin <oriokie@icloud.com>
2023-07-26 22:06:00 +0300

    Try this one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,19 +8,22 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	/* initializing variables */
-	int i = 0;
-	int j = 0;
+	int i, j;
+
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0')
+
+	while (src[j] != '\0' && j < n)
 	{
-		if (j < n)
-			dest[i] = src[j];
-			j++;
-			i++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
