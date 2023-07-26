5582f17bd448635ebdd54017f5870aac62c3a5d3
“Edwin <oriokie@icloud.com>
2023-07-26 22:20:42 +0300

    Edited the code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,21 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
