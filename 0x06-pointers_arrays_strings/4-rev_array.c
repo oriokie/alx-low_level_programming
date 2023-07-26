4e5bc9c764cc916ccae681bf10af5469b4204dab
“Edwin <oriokie@icloud.com>
2023-07-26 22:29:23 +0300

    Lets reverse arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* reverse_array - function that reverses an array
+* @a: an array of integers
+* @n: number of elements of the array to swap
+* Return: nothing
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp;
+
+	for (; i > j; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
