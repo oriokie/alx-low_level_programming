b6c369d470c9d305a53af05f8afe9c829c63135d
“Edwin <oriokie@icloud.com>
2023-07-26 09:48:34 +0300

    Added int n to the concat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncat - function that concenates two strings
+* @dest: destination string
+* @src: source string
+* @n: limiter
+* Return: destination as a pointer
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	/* initializing variables */
+	int i = 0, int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		if (j < n)
+		{
+			dest[i] = src[j];
+			j++;
+			i++;
+		}
+	dest[i] = '\0';
+	return (dest);
+}
