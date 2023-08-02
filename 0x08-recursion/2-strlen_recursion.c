1ef9b79b3ffe226d09858d926c8e0476842316be
“Edwin <oriokie@icloud.com>
2023-08-02 21:12:53 +0300

    Lets return the length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that return the length of string s
+ * @s: the string under question
+ * Return: Always zero
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	/**BASE CASE - end of the string */
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+	/*Recursively calling the function */
+	len++;
+	_strlen_recursion(s + 1);
+	}
+	return (1);
+}
