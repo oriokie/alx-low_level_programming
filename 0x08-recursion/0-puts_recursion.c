bf7980e1fe513d0ee3f0a164c486490fe6a35d39
“Edwin <oriokie@icloud.com>
2023-08-02 20:37:05 +0300

    Using recursion to print a line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a sting and then a new line
+ * @s: this is the sting to be printed
+ * Return: Always zero
+ */
+void _puts_recursion(char *s)
+{
+	/* The base of the recursion program */
+	if (*s == '\n')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	/*Recursion margic */
+
+	_putchar(*s)
+	_puts_recursion(s + 1);
+}
