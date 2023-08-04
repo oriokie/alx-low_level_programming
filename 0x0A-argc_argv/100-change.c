73fd413513d41a1185d1e0072692c3c5aa7cab8d
“Edwin <oriokie@icloud.com>
2023-08-05 00:28:42 +0300

    edited the error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - This is the entry point 
+ * main - This is the entry point
  * @argc: count of arguments
  * @argv: the arguments
  * Return: Zero for success
