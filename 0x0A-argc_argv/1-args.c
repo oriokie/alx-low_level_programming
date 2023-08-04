11bc7a258d4fc9240c8c470af90e34c1ca291d6e
“Edwin <oriokie@icloud.com>
2023-08-04 22:23:54 +0300

    Lets print the number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point to a program that prints the number of arguments passed
+ * into it
+ * @argc: the number of arguments
+ * @argv: the pointer containing the arguments entered
+ * Return: Awalys 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
