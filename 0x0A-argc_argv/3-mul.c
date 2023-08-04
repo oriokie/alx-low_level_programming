769e237025a8473a7943923cb1978d474af1d182
“Edwin <oriokie@icloud.com>
2023-08-04 22:47:39 +0300

    Lets multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point for the function that multiplies two numbers
+ * @argc: count of the parameters
+ * @argv: the parameters
+ * Return: zero for success and one for error
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+
+		b = atoi(argv[2]);
+
+		result = a * b;
+
+		printf("%d\n", result);
+
+		return (0);
+	}
+}
