a344fb57ce69319622471c5e4ab28ae063a9e906
“Edwin <oriokie@icloud.com>
2023-08-07 22:06:18 +0300

    Malloc is fun with counters

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * counter - prototype for the counter function not included in the header file
+ * @a: pointer
+ * Return: None
+ */
+int counter(char *a);
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in the
+ * memory. It should contain a copy of the string given as parameter
+ * @str: string to be passed as parametern
+ * Return: Null if the string the null also NULL if there is insufficient
+ * memory
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *arr;
+	int size;
+
+	size = counter(str);
+
+	arr = malloc(sizeof(char) * (size + 1));
+
+	if (arr != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			arr[i] = str[i];
+		}
+		arr[i] = '\0';
+		return (arr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+/**
+ * counter - functions that counts a string and returns the count of characters
+ * @a: the string to be counted
+ * Return: the count
+ */
+int counter(char *a)
+{
+	int i = 0;
+	int count = 0;
+
+	for (; a[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
