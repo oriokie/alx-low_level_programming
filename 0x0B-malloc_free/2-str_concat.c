a1cd54fa0c46efae1e4ca219a3ad87d9fcf74cd3
“Edwin <oriokie@icloud.com>
2023-08-08 20:16:39 +0300

    Lets concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * counter - this is a protype
+ * @a: string to be counted
+ * Return: none
+ */
+int counter(char *a);
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: array with the concenatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int k, size_s1, size_s2, total_size;
+	char *arr = NULL;
+
+	k = 0;
+
+	size_s1 = counter(s1);
+	size_s2 = counter(s2);
+	total_size = size_s1 + size_s2;
+	arr = malloc(sizeof(char) * (total_size + 1));
+	if (arr != NULL)
+	{
+		while (*s1 != '\0')
+		{
+			arr[k] = *s1;
+			k++;
+			s1++;
+		}
+		while (*s2 != '\0')
+		{
+			arr[k] = *s2;
+			k++;
+			s2++;
+		}
+		arr[k] = '\0';
+		return (arr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+/**
+ * counter - functions that counts a string and returns the count of characters
+ * @a: the string to be counted
+ * Return: the count
+ */
+int counter(char *a)
+{
+	int i = 0;
+	int count = 0;
+
+	for (; a[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
