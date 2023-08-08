edfc3d0d5c1eea042c05082c4e70706b585d6df8
“Edwin <oriokie@icloud.com>
2023-08-08 22:50:27 +0300

    Lets free the memomry

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - function that frees the memory allocated by the alloc_grid
+ * function
+ * @grid: the grid to free
+ * @height: the height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid == NUll || height == NULL)
+		return;
+
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
