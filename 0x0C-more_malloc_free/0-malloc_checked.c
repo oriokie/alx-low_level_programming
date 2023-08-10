34f73b38839424f023425a871200e7e8cb1aff42
“Edwin <oriokie@icloud.com>
2023-08-10 21:49:20 +0300

    Lets allocate memory auto

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *malloc_checked - function that allocates memory based on the variable b
+ * @b: variable indicating the size of memory to be allocated
+ * Return: if fail return 98, success return a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+}
