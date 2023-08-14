00d7fd875b4149b6149ed2d1b4e46b2f0bb1f376
“Edwin <oriokie@icloud.com>
2023-08-14 21:30:44 +0300

    Lets make the header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MY_P
+#define MY_P
+#include <stdio.h>
+/**
+ * struct dog - struct declaration for my dog
+ * @name: the name of my dog
+ * @age: the age of the dog
+ * @ownner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *name;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
