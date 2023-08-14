c30ba9d20b2de098e7ef4f4b83db38d68e42218d
“Edwin <oriokie@icloud.com>
2023-08-14 20:42:33 +0300

    fixed issue 3

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #ifndef name
 #define name __FILE__
 #endif
+#include <stdio.h>
 /**
  * main - this program prints the name of the file it was compiled from.
  * Return: Always zero
  */
-int main (void)
+int main(void)
 {
 	printf("%s\n", name);
 	return (0);
