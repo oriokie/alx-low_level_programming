32263da7b1058f8629ea570a25428f1b931ccb75
“Edwin <oriokie@icloud.com>
2023-08-14 20:38:47 +0300

    Main function to print out the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef name
+#define name __FILE__
+#endif
+/**
+ * main - this program prints the name of the file it was compiled from.
+ * Return: Always zero
+ */
+int main (void)
+{
+	printf("%s\n", name);
+	return (0);
+}
