ccfc07a4c54fc77e098b3d8c0d980c8d29a99701
“Edwin <oriokie@icloud.com>
2023-08-16 20:02:23 +0300

    Done with the function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: the array to be searched
+ * @size: number of items in the array to be searched
+ * @cmp: the pointer to the function that will be used to compare values
+ * Return: -1 for items not matching, and 1 if matching. -1 if size is less
+ * than or equal to zero
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int counter;
+
+	if (array && cmp)
+	{
+		counter = 0;
+
+		while (counter < size)
+		{
+			if (cmp(array[counter]) != 0)
+			{
+				return (counter);
+				counter++;
+			}
+		}
+	}
+	return (-1);
+}
