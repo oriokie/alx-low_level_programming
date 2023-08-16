776bc55bb2aa2f6b384b285e03c0279e1afadeec
“Edwin <oriokie@icloud.com>
2023-08-16 19:43:00 +0300

    Done with the code

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function given a parameter of each
+ * element of an array
+ * @array: array of numbers
+ * @size: size of the array
+ * @action: this is the pointer to the function that we need to use
+ * Return: Nothing (void)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	if (array && size && action)
+	{
+		a = 0;
+
+		while (a < size)
+		{
+			action(array[1]);
+			a++;
+		}
+	}
+}
