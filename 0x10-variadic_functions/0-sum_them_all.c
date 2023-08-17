2dcf8e259500a7bdb46aab3e02b19b4908d62afd
“Edwin <oriokie@icloud.com>
2023-08-17 22:16:15 +0300

    Done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 /**
  * sum_them_all - a function that sums all the given paramaters
  * @n: arguments passsed to function
@@ -12,18 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if (n != 0)
+	if (n == 0)
+		return (0);
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(list, n);
-
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(list, int);
-		}
-
-		va_end(list);
-
-		return (sum);
+		sum += va_arg(list, int);
 	}
-	return (0);
+	va_end(list);
+
+	return (sum);
 }
