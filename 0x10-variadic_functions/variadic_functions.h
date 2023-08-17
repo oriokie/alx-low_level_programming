723356dbdea8f98d049484fd26b3f528a5ca8c43
“Edwin <oriokie@icloud.com>
2023-08-17 22:16:24 +0300

    Done with the code

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VAR
+#define VAR
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
