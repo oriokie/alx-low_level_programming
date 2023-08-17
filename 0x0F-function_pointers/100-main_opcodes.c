aba2cd6fd39199f761e6d86cd4ba5a303748d7c7
“Edwin <oriokie@icloud.com>
2023-08-17 05:09:41 +0300

    Done with the opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	int i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx ", arr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
