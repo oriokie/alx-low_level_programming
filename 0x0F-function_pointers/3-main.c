9730fe0d9fc58f0e69dff83af738fb1ab2277cde
“Edwin <oriokie@icloud.com>
2023-08-17 04:48:58 +0300

    Done with the calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - Main Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the arguments
+ * Return: 0 on success, 98 for incorrect number of arguments,
+ *         99 for invalid operator, and appropriate result for calculation.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(operator);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	printf("%d\n", operation(num1, num2));
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - function that returns the sum of a and b
+ * @a: First integer
+ * @b: Second integer
+ * Return: the sum of integer a and b
+ **/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that finds the difference of a and b
+ * @a: First integer
+ * @b: Second integer
+ * Return: the difference between a and b
+ **/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that returns the product of a and b
+ * @a: First integer
+ * @b: Second integer
+ * Return: product of a and b
+ **/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that return the division of a and b
+ * @a: First integer
+ * @b: Second integer
+ * Return: result of the division between a and b
+ **/
+
+int op_div(int a, int b)
+{
+	if (b  <= 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the reminder of the division of a and b
+ * @a: First  integer
+ * @b: Second integer
+ * Return: remainder of division of a and b
+ **/
+
+int op_mod(int a, int b)
+{
+	if (b  <= 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
