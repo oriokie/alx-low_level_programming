c101162eebb4429f4243f5ed36fe1396fc76b883
“Edwin <oriokie@icloud.com>
2023-08-25 16:14:20 +0300

    added the 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,8 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	/**initialize a new variable as per our user defined struct */
 
-	list_t *new, *tmp;
+	list_t *new;
+	list_t *tmp;
 
 	/** allocate memory for our variable */
 
@@ -22,11 +23,13 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = strlen(len);
+	new->len = strlen(str);
 	new->next = NULL;
 
+	tmp = *head;
+
 	if (tmp == NULL)
-		*head = new
+		*head = new;
 	else
 	{
 		while (tmp->next != NULL)
@@ -35,5 +38,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		tmp->next = new;
 	}
-	return (*head)
+	return (*head);
 }
