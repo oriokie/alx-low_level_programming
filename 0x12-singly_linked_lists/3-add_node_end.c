8cbc465222e7d77c0f7f5d74a3e103b8324ecc42
“Edwin <oriokie@icloud.com>
2023-08-25 16:09:26 +0300

    added the 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * add_node_end - function that adds a new node at the end of a list_t list
+ * @head: this is the head of the string
+ * @str: the string to be read
+ *
+ * Return: the address of the new element. Return NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	/**initialize a new variable as per our user defined struct */
+
+	list_t *new, *tmp;
+
+	/** allocate memory for our variable */
+
+	new = malloc(sizeof(list_t));
+
+	/** check if it is null */
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(len);
+	new->next = NULL;
+
+	if (tmp == NULL)
+		*head = new
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+	return (*head)
+}
