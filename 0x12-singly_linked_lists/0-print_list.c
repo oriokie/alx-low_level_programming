3c5cccf1010c3e09086811bc250dbc7287450005
“Edwin <oriokie@icloud.com>
2023-08-25 14:54:59 +0300

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	unsigned long int count = 0;
 
 
 	while (h != NULL)
 	{
-		if (h->str == NULL || h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nill)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%lu] %s\n", h->len, h->str);
 
 		h = h->next;
 		count++;
