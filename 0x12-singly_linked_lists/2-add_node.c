c55886b0ec4cd9779700f331a793d3c86e613592
“Edwin <oriokie@icloud.com>
2023-08-25 15:38:42 +0300

    used strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = _strlen(str);
+	new->len = strlen(str);
 	new->next = (*head);
 
 	(*head) = new;
