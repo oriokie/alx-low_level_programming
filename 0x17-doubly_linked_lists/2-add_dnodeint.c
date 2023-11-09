9f212722c436f6e3b6bd0685944fde36d75ae6b9
“Edwin <oriokie@icloud.com>
2023-11-09 12:50:47 +0300

    lets add a node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function that adds a node at the beginning of a list
+ * @head: the first element of the list
+ * @n: the data to be stored in the node
+ *
+ * Return: the address of the new element; NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	
+	new = malloc(sizeof(dlistint_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+	if (!head)
+	{
+		free(new);
+		return(NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
