470001444bf6e9abfdfdce6e3873267b43b14dc8
“Edwin <oriokie@icloud.com>
2023-11-09 12:52:06 +0300

    betty fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (!new)
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!head)
 	{
 		free(new);
-		return(NULL);
+		return (NULL);
 	}
 	new->n = n;
 	new->next = *head;
