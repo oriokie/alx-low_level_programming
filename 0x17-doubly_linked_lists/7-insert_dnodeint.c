0320b17a71c4813cd98f59b9e5f022a09b632ca3
“Edwin <oriokie@icloud.com>
2023-11-10 12:04:16 +0300

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0) /* insert the new node at the beginning of the list */
 	{
 		new = add_dnodeint(h, n);
+		return (new);
 	}
 	for (i = 0; i < idx - 1 && current != NULL; i++) /* searching for the index */
 	{
