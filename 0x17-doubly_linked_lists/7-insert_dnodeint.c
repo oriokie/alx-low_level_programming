5486baa7e37ed7b014f37b9844bc743fa99900f3
“Edwin <oriokie@icloud.com>
2023-11-10 05:25:04 +0300

    insertion

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function that inserts a new node at a given
+ * position
+ * @h: the head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node or NULL for failure
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *current = *h;
+	unsigned int i;
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t)); /* allocating memory to the new node */
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n; /* initializing the new node */
+	new->next = NULL;
+	new->prev = NULL;
+	if (idx == 0) /* insert the new node at the beginning of the list */
+	{
+		*h = new;
+		return (new);
+	}
+	for (i = 0; i < idx && current != NULL; i++) /* searching for the index */
+	{
+		current = current->next;
+	}
+	if (current == NULL) /* reached the end before we find the index */
+	{
+		free(new);
+		return (NULL);
+	}
+	if (current->next == NULL) /* is the end of the list? */
+	{
+		add_dnodeint_end(h, n);
+		return (new);
+	}
+	new->next = current->next; /*otherwise, insert middle */
+	new->prev = current;
+	current->next->prev = new;
+	current->next = new;
+	return (new);
+}
