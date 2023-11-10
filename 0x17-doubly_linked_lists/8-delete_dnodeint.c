f477e022b0a89254265fa5a99457d2a1b314a441
“Edwin <oriokie@icloud.com>
2023-11-10 05:49:31 +0300

    lets delete

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: head of the list
+ * @index: index of the node to be deleted
+ *
+ * Return: 1 for success and -1 for failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	for (i = 0; current != NULL && i < index; ++i)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+		return (-1);
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
