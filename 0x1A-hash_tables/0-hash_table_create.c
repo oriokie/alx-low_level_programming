9fb0cbaf1cb87b50f4510ec241d268e5827d5e0a
“Edwin <oriokie@icloud.com>
2023-12-01 06:57:52 +0300

    created hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ * Return: NULL if something went wrong
+ * Otherwise - a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* Declaring a pointer to the hash table structure */
+	hash_table_t *hash_table;
+
+	/* Allocating memory for the hash table structure */
+	hash_table = calloc(1, sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return (NULL);
+
+	/* Allocating memory for the array within the hash table */
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	/* Setting the size of the array within the hash table */
+	hash_table->size = size;
+
+	/*Returning the pointer to the newly created hash table*/
+	return (hash_table);
+}
